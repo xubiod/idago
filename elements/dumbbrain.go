a1531e81eb972ed5d46ebafb4c3c791be8727407
xubiod <xubiod.mail@gmail.com>
2022-07-18 10:36:53 -0400

    lamo who needs research when just playin around

--- elements/dumbbrain.go
+++ elements/dumbbrain.go
@@ -1,5 +1,7 @@
 package idago
 
+import "errors"
+
 // Note absolutely no experience with neural networks or machine learning has
 // gone into this and don't feel like doing any as well :)
 
@@ -15,6 +17,22 @@ type Layer []Axon
 
 type DumbBrain []Layer
 
+func (into *Axon) Merge(with *Axon, preference float32) {
+	(*into).Multiplier = (((*into).Multiplier * (1 - preference)) + ((*with).Multiplier * preference))
+}
+
+func (into *Layer) Merge(with *Layer, preference float32) error {
+	if len(*into) != len(*with) {
+		return errors.New("dumbbrain: axon count in layer mismatch, no changes done")
+	}
+
+	for intoIndex, intoAxon := range *into {
+		intoAxon.Merge(&(*with)[intoIndex], preference)
+	}
+
+	return nil
+}
+
 func (layer *Layer) Passdown() {
 	var fromVal float32
 	for _, axon := range []Axon(*layer) {
@@ -27,10 +45,19 @@ func (layer *Layer) Passdown() {
 	}
 }
 
-// func (into *DumbBrain) Merge(with *DumbBrain, preference float32) {
-// 	for layerIndex, layer := range []Layer(*with) {
-// 		for axonIndex, axon := range []Axon(layer) {
+func (network *DumbBrain) Runthrough() {
+	for _, layer := range []Layer(*network) {
+		layer.Passdown()
+	}
+}
 
-// 		}
-// 	}
-// }
+func (into *DumbBrain) Merge(with *DumbBrain, preference float32) error {
+	if len(*into) != len(*with) {
+		return errors.New("dumbbrain: layer count mismatch, no changes done")
+	}
+
+	for intoIndex, _ := range *into {
+		(*into)[intoIndex].Merge(&(*with)[intoIndex], preference)
+	}
+	return nil
+}
